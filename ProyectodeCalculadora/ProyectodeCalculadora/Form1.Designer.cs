158c0131ae90441634177338ac4aa45d1ae84ca2
Yeimy Chávez <yeimyjulissa006@gmail.com>
2025-07-30 20:35:19 -0600

    Calculadora Básica de suma de dos números

---
+++ ProyectodeCalculadora/ProyectodeCalculadora/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace ProyectodeCalculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtNum1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtNum1.Location = new System.Drawing.Point(135, 82);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(116, 20);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.ForeColor = System.Drawing.Color.Teal;
+            this.lblNum1.Location = new System.Drawing.Point(77, 76);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(48, 24);
+            this.lblNum1.TabIndex = 1;
+            this.lblNum1.Text = "Num 1:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.Black;
+            this.btnCalcular.Location = new System.Drawing.Point(135, 258);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(83, 31);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum2.ForeColor = System.Drawing.Color.Teal;
+            this.lblNum2.Location = new System.Drawing.Point(77, 123);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(52, 24);
+            this.lblNum2.TabIndex = 4;
+            this.lblNum2.Text = "Num 2:";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtNum2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtNum2.Location = new System.Drawing.Point(135, 129);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(116, 20);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuesta.ForeColor = System.Drawing.Color.Teal;
+            this.lblRespuesta.Location = new System.Drawing.Point(77, 183);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(85, 24);
+            this.lblRespuesta.TabIndex = 5;
+            this.lblRespuesta.Text = "Respuesta: ?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 348);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label lblRespuesta;
+    }
+}
+
---
+++ ProyectodeCalculadora/ProyectodeCalculadora/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectodeCalculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double num1, num2, respuesta;
+            num1 = double.Parse(txtNum1.Text);
+
+            num2 = double.Parse(txtNum2.Text);
+
+            respuesta = num1 + num2;
+            lblRespuesta.Text = "Respuesta: " + respuesta;
+        }
+    }
+}
