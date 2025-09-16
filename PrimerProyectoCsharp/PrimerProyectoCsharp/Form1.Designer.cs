bace6e8f07522214a178f952511ecf5053f162b8
Yeimy Chávez <yeimyjulissa006@gmail.com>
2025-09-15 19:54:58 -0600

    Creación de la nueva rama Académica para cómputo II, POO y bases de datos

--- PrimerProyectoCsharp/PrimerProyectoCsharp/Form1.Designer.cs
+++ PrimerProyectoCsharp/PrimerProyectoCsharp/Form1.Designer.cs
@@ -29,97 +29,20 @@ namespace PrimerProyectoCsharp
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblSerie = new System.Windows.Forms.Label();
-            this.btnCalcular = new System.Windows.Forms.Button();
-            this.lblMedia = new System.Windows.Forms.Label();
-            this.lblTipica = new System.Windows.Forms.Label();
-            this.lblArmonica = new System.Windows.Forms.Label();
-            this.txtSerie = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // lblSerie
-            // 
-            this.lblSerie.AutoSize = true;
-            this.lblSerie.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSerie.Location = new System.Drawing.Point(58, 41);
-            this.lblSerie.Name = "lblSerie";
-            this.lblSerie.Size = new System.Drawing.Size(48, 16);
-            this.lblSerie.TabIndex = 0;
-            this.lblSerie.Text = "Serie:";
-            // 
-            // btnCalcular
-            // 
-            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalcular.Location = new System.Drawing.Point(107, 213);
-            this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(83, 28);
-            this.btnCalcular.TabIndex = 1;
-            this.btnCalcular.Text = "Calcular";
-            this.btnCalcular.UseVisualStyleBackColor = true;
-            // 
-            // lblMedia
-            // 
-            this.lblMedia.AutoSize = true;
-            this.lblMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMedia.Location = new System.Drawing.Point(54, 101);
-            this.lblMedia.Name = "lblMedia";
-            this.lblMedia.Size = new System.Drawing.Size(54, 16);
-            this.lblMedia.TabIndex = 3;
-            this.lblMedia.Text = "Media:";
-            // 
-            // lblTipica
-            // 
-            this.lblTipica.AutoSize = true;
-            this.lblTipica.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTipica.Location = new System.Drawing.Point(53, 128);
-            this.lblTipica.Name = "lblTipica";
-            this.lblTipica.Size = new System.Drawing.Size(55, 16);
-            this.lblTipica.TabIndex = 5;
-            this.lblTipica.Text = "Típica:";
-            // 
-            // lblArmonica
-            // 
-            this.lblArmonica.AutoSize = true;
-            this.lblArmonica.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblArmonica.Location = new System.Drawing.Point(28, 155);
-            this.lblArmonica.Name = "lblArmonica";
-            this.lblArmonica.Size = new System.Drawing.Size(80, 16);
-            this.lblArmonica.TabIndex = 7;
-            this.lblArmonica.Text = "Armonica: ";
-            // 
-            // txtSerie
-            // 
-            this.txtSerie.Location = new System.Drawing.Point(107, 41);
-            this.txtSerie.Name = "txtSerie";
-            this.txtSerie.Size = new System.Drawing.Size(121, 20);
-            this.txtSerie.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(310, 286);
-            this.Controls.Add(this.txtSerie);
-            this.Controls.Add(this.lblArmonica);
-            this.Controls.Add(this.lblTipica);
-            this.Controls.Add(this.lblMedia);
-            this.Controls.Add(this.btnCalcular);
-            this.Controls.Add(this.lblSerie);
+            this.ClientSize = new System.Drawing.Size(366, 286);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ADMINISTRACIÓN DE ALUMNOS";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblSerie;
-        private System.Windows.Forms.Button btnCalcular;
-        private System.Windows.Forms.Label lblMedia;
-        private System.Windows.Forms.Label lblTipica;
-        private System.Windows.Forms.Label lblArmonica;
-        private System.Windows.Forms.TextBox txtSerie;
     }
 }
 
