b9cbd352f087e6e5fbd6a80d22bfc04751a1d72e
JEFFERSON STEVEN CASTILLO LEIVA <jeffersonk20castillo@gmail.com>
2025-10-25 10:51:50 -0600

    inicio proyecto

---
+++ proyectofinal/proyectofinal/Models/MydbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace proyectofinal.Models
+{
+    public class MydbContext : DbContext
+    {
+        public MydbContext(DbContextOptions<MydbContext> options) : base(options) { }
+        public DbSet<usuarioPaciente> usuarioPaciente { get; set; }
+
+        //Conexion a la base de datos; a la tabla usuarioPaciente.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<usuarioPaciente>().HasKey(a =>a.idPacienteUsuario);
+        }
+    }
+}
---
+++ proyectofinal/proyectofinal/Models/usuarioPaciente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace proyectofinal.Models
+{
+    public class usuarioPaciente
+    {
+        [Key]
+        public int idPacienteUsuario { get; set; }
+
+        public char email { get; set; }
+        public string password { get; set; } = string.Empty;  
+
+    }
+}
