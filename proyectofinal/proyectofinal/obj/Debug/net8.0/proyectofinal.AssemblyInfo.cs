2b13ebff4cef1c9d1cc7eb848b43bdc766143a89
JEFFERSON STEVEN CASTILLO LEIVA <jeffersonk20castillo@gmail.com>
2025-11-19 18:31:10 -0600

    subAgendacion.html ya guarda los datos en ambas tablas

--- proyectofinal/proyectofinal/obj/Debug/net8.0/proyectofinal.AssemblyInfo.cs
+++ proyectofinal/proyectofinal/obj/Debug/net8.0/proyectofinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectofinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dfab739d22105d432f4c524d490914e1e66b1a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c52043add430eaab5502c4cc21dc0d25633755")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectofinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectofinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
