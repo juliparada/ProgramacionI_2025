432df28cdba27c09617f0ad915bde27ae6c8fc7a
Yeimy Chávez <yeimyjulissa006@gmail.com>
2025-11-26 18:37:55 -0600

    Creacion del formulario materia y docente

---
+++ WebAppAcademica/WebAppAcademica/Models/DocenteBusquedaParametros.cs
@@ -0,0 +1,7 @@
+﻿namespace WebAppAcademica.Models
+{
+    public class DocenteBusquedaParametros
+    {
+        public string? buscar { get; set; }
+    }
+}
---
+++ WebAppAcademica/WebAppAcademica/Models/MateriaBusquedaParametros.cs
@@ -0,0 +1,7 @@
+﻿namespace WebAppAcademica.Models
+{
+    public class MateriaBusquedaParametros
+    {
+        public string? buscar { get; set; }
+    }
+}
