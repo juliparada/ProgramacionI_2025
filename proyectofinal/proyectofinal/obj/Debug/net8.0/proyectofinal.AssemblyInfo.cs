45f8bb2aff2968f34a4025c8f1d5097113186292
JEFFERSON STEVEN CASTILLO LEIVA <jeffersonk20castillo@gmail.com>
2025-11-30 16:41:44 -0600

    Se agregó las limitaciones en DatosPacientes.html si el paciente es menor de edad.

--- proyectofinal/proyectofinal/obj/Debug/net8.0/proyectofinal.AssemblyInfo.cs
+++ proyectofinal/proyectofinal/obj/Debug/net8.0/proyectofinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectofinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db0cb9fd35dcf52c3779e5ba5ddbc8711dfea800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6aaf36f20149a7a0cdf8808313a15be6ec219e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectofinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectofinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
